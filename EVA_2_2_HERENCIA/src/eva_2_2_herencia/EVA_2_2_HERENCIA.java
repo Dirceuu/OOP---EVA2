91adb1a2018a267f837475e1cb3c4f0fc631ab41
Dirceu <124701508+Dirceuu@users.noreply.github.com>
2023-03-14 00:22:27 -0600

    Add files via upload

---
+++ EVA_2_2_HERENCIA/src/eva_2_2_herencia/EVA_2_2_HERENCIA.java
@@ -0,0 +1,16 @@
+package eva_2_2_herencia;
+
+/**
+ *
+ * @author Alan
+ */
+public class EVA_2_2_HERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ EVA_2_2_HERENCIA/src/eva_2_2_herencia/Proveedores.java
@@ -0,0 +1,55 @@
+package eva_2_2_herencia;
+
+/**
+ *
+ * @author Alan
+ */
+public class Proveedores {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String razonSocial;
+    private String rfc;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+    
+        
+}
