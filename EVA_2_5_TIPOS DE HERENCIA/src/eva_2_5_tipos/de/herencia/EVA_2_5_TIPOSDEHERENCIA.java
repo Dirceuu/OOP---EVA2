50409e3d92fa5c0df1fec87ca219f847b8ab4277
Dirceu <124701508+Dirceuu@users.noreply.github.com>
2023-03-15 22:19:03 -0600

    Add files via upload

---
+++ EVA_2_5_TIPOS DE HERENCIA/src/eva_2_5_tipos/de/herencia/EVA_2_5_TIPOSDEHERENCIA.java
@@ -0,0 +1,30 @@
+package eva_2_5_tipos.de.herencia;
+
+/**
+ *
+ * @author Alan
+ */
+public class EVA_2_5_TIPOSDEHERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    class SuperClase{
+        
+    }
+    
+    class OtraSuperClase{ //no se puede heredar de dos o mas superclases
+        
+    }
+    
+    //extends solo funciona para una unica clase java solo soporta una herencia simple
+    
+    class BaseClase extends SuperClase{
+        
+    }
+    
+}
