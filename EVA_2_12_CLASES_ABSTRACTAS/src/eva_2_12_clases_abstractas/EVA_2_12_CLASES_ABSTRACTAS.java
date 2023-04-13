c1c1dac5a8bdfa9fc10dccd39018beb5782a375d
Dirceu <124701508+Dirceuu@users.noreply.github.com>
2023-04-13 14:48:08 -0600

    Add files via upload

---
+++ EVA_2_12_CLASES_ABSTRACTAS/src/eva_2_12_clases_abstractas/EVA_2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_12_clases_abstractas;
+
+/**
+ *
+ * @author Alan
+ */
+public class EVA_2_12_CLASES_ABSTRACTAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Prueba prueba = new Prueba();
+        //PruebaAbstracta pAbs = new PruebaAbstracta(); 
+        //NO SE PUEDEN CREAR OBJETOS DE LA CLASE ABSTRACTA
+        
+    } 
+    
+}
+
+class Prueba {
+        
+    }
+
+abstract class PruebaAbstracta{
+    
+}
